af84e63c0b7fad2bc568180efb57e08610be7470
chetaofeng <chetaofeng@163.com>
2016-05-26 01:37:11 +0800

    添加评价按钮的动画效果

--- MyPinFood/MyPinFood/ResturantReviewViewController.swift
+++ MyPinFood/MyPinFood/ResturantReviewViewController.swift
@@ -11,12 +11,34 @@ import UIKit
 class ResturantReviewViewController: UIViewController {
 
     @IBOutlet weak var backResutrantImage: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style:.Light))
         blurEffectView.frame = view.frame
         backResutrantImage.addSubview(blurEffectView)
+        
+        //先设置stackview的大小为0
+        let scale = CGAffineTransformMakeScale(0, 0) //缩放
+        let translate = CGAffineTransformMakeTranslation(0, 500)
+        
+        stackView.transform = scale
+        stackView.transform = translate
+    }
+    
+    //界面显示的时候添加动画效果
+    override func viewDidAppear(animated: Bool) {
+//        //在设置补帧效果
+//        UIView.animateWithDuration(1) {
+//            self.stackView.transform = CGAffineTransformIdentity //1s之后到尾帧
+//        }
+        
+        //设置补帧效果二
+        UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.5, options: [], animations: {
+             self.stackView.transform = CGAffineTransformIdentity //1s之后到尾帧
+            }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
