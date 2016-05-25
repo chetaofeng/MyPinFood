1b45c42dc184e63a46882453d11a7fb94eb1284b
chetaofeng <chetaofeng@163.com>
2016-05-25 14:34:03 +0800

    以class的方式整理代码

--- MyPinFood/MyPinFood/Resturant.swift
+++ MyPinFood/MyPinFood/Resturant.swift
@@ -8,10 +8,28 @@
 
 import Foundation
 
+//此处可以使用结构体和类两种方式，推荐用结构体
+
 struct ResutrantStruct {
     var name:String
     var type:String
     var location:String
     var image:String
     var isHasVisited:Bool
+}
+
+class Resutrant {
+    var name:String
+    var type:String
+    var location:String
+    var image:String
+    var isHasVisited:Bool
+    
+    init(name:String,type:String,location:String,image:String,isHasVisited:Bool){
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isHasVisited = isHasVisited
+    }
 }
