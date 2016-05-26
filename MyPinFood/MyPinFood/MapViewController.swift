ed66b57f0a584c40a5daaba1903e227efbe08b93
chetaofeng <chetaofeng@163.com>
2016-05-26 11:18:15 +0800

    添加自定义标注，有缩略图

--- MyPinFood/MyPinFood/MapViewController.swift
+++ MyPinFood/MyPinFood/MapViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController ,MKMapViewDelegate{
     
     var resturantStruct:ResutrantStruct!
     
@@ -19,7 +19,6 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
 
         //地址转换
-        
         let geoCoder = CLGeocoder()
         geoCoder.geocodeAddressString(resturantStruct.location) { (placeMarks, error) in
             if error != nil{
@@ -57,5 +56,27 @@ class MapViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    //自定义地图的标注
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let id = "我的图钉"
+        
+        if annotation is MKUserLocation {   //用户位置视图也是一个标注视图，排除对其的定制
+            return nil
+        }
+        
+        //为性能考虑，重用一个已存在的标注视图，如果不存在，则创建
+        var annotationView = self.mapView.dequeueReusableAnnotationViewWithIdentifier(id) as? MKPinAnnotationView
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: id)
+            annotationView?.canShowCallout = true //启用气球提示方式
+        }
+        
+        //添加缩略图
+        let leftIconView = UIImageView(frame: CGRectMake(0, 0, 53, 53))
+        leftIconView.image = UIImage(named: self.resturantStruct.image)
+        annotationView?.leftCalloutAccessoryView = leftIconView
+        
+        return annotationView
+    }
 }
